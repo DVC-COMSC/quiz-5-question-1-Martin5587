8f14824e1e09bdd3b9ae1d54c23d18d0385a5403
Martin5587 <mcontreras265@insite.4cd.edu>
2024-03-07 20:35:43 -0800

    commit(1)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include    <iostream>
 using namespace std;
 
+int letterRowTriangle(int rows) {
+   for (int i = 1; i <= rows; ++i) {
+      char letter = 'A';
+      for (int j = 0; j < i; ++j) {
+         cout << letter++;
+         if (j < i - 1) cout << " ";
+      }
+      cout << endl;
+   }
+}
+
 int main()
 {
    
-   /* Complete your code */
   
 }
